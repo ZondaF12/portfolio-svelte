b0cd7ab7c92c243843a534ce66a074e75b736ffe
livecoderuaridh <ruaridhbell73@googlemail.com>
2025-11-03 21:45:36 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<div class="flex h-screen items-center justify-center">
+	<div class="flex flex-col items-start justify-center gap-6">
+		<div class="h-32 w-32 overflow-hidden rounded-full">
+			<img
+				src="/Ruaridh.JPG"
+				alt="Favicon"
+				class="h-full w-full scale-300 object-cover object-bottom"
+			/>
+		</div>
+		<h1>Ruaridh Bell</h1>
+	</div>
+</div>
